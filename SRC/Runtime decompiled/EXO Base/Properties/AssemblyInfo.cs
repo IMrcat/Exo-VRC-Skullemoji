b14e76ccbd3a5ce0f0a524dcb829f4d83ec0c2d4
Cat <104047478+IMrcat@users.noreply.github.com>
2025-03-10 05:18:24 -0400

    Skided

---
+++ SRC/Runtime decompiled/EXO Base/Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.Versioning;
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyCompany("EXO Base")]
+[assembly: AssemblyConfiguration("Debug")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0+ef5f7c1179d8eee0b1a40de86bca10667f52089f")]
+[assembly: AssemblyProduct("EXO Base")]
+[assembly: AssemblyTitle("EXO Base")]
+[module: RefSafetyRules(11)]
